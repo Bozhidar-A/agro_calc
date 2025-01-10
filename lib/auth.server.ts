62600ef5a4c08ca0ff80e6f263cfcd86be28a2a1
Bozhidar-A <bplatanasov@gmail.com>
2025-01-10 05:46:31 +0200

    needed packages and auth.server removed wrong call

--- lib/auth.server.ts
+++ lib/auth.server.ts
@@ -1,5 +1,4 @@
-import { verify } from "jsonwebtoken";
-import { prisma } from "@/lib/prisma";
+import { jwtVerify } from "jose";
 
 export async function VerifyTokenServer(secret: string, token: string, type: string) {
     if (!token) {
@@ -7,27 +6,31 @@ export async function VerifyTokenServer(secret: string, token: string, type: str
     }
 
     // Verify refresh token
-    const decoded = verify(token, secret);
+    console.log("VerifyTokenServer -> token", token)
+    console.log("VerifyTokenServer -> secret", secret)
+    const decoded = await jwtVerify(token, new TextEncoder().encode(secret));
+
+    console.log("VerifyTokenServer -> decoded", decoded)
 
 
-    if (decoded.type !== type) {
+    console.log("VerifyTokenServer -> type", type)
+    if (decoded?.payload?.type !== type) {
+        console.log("Invalid token type")
         return [false, null];
     }
 
     // Check if refresh token exists in database
-    const storedToken = await prisma.refreshToken.findFirst({
-        where: {
-            token: token,
-            userId: decoded.userId,
-            expiresAt: {
-                gt: new Date()
-            }
-        }
-    });
+    // const storedToken = await prisma.refreshToken.findFirst({
+    //     where: {
+    //         // id: decoded.payload,
+    //         token
+    //     }
+    // });
 
-    if (!storedToken) {
-        return [false, null];
-    }
+    // console.log("VerifyTokenServer -> storedToken", storedToken)
+    // if (!storedToken) {
+    //     return [false, null];
+    // }
 
     return [true, decoded];
 }
