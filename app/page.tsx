39ec0a18a1a9837f3c68a12a5ea31855d8ae18db
Bozhidar-A <bplatanasov@gmail.com>
2025-01-13 01:40:39 +0200

    more ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 import { useSelector } from 'react-redux';
-import LogoutButton from '@/components/LogoutButton/LogoutButton';
 
 export default function HomePage() {
   const authObj = useSelector((state) => state.auth);
@@ -13,9 +12,6 @@ export default function HomePage() {
     <>
       <Welcome />
       <ColorSchemeToggle />
-      <Link href="auth/login">Login</Link>
-      <Link href="auth/register">Register</Link>
-      <LogoutButton />
       <Link href="/prot">Test Prot</Link>
       <Link href="/idk">Test Non</Link>
       <p>{JSON.stringify(authObj)}</p>
