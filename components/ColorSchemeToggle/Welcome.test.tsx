64349ef9061f6a73f3cce33ab416321ac6f764f8
Bozhidar-A <bplatanasov@gmail.com>
2025-02-06 23:23:57 +0200

    swapped mantine for shadcn, did not like style api

--- components/ColorSchemeToggle/Welcome.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@/test-utils';
-import { ColorSchemeToggle } from './ColorSchemeToggle';
-
-describe('ColorSchemeToggle component', () => {
-  it('has Auto text', () => {
-    render(<ColorSchemeToggle />);
-    expect(screen.getByText('Auto'));
-  });
-});
--- components/LangList/LangList.Mantine.ts
+++
@@ -1,5 +0,0 @@
-export const styles = {
-    groupFull: {
-        justify: "space-between", h: "100%", w: "100%"
-    }
-}
