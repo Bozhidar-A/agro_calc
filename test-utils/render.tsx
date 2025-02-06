9a25a0fb3e8d2eeb444e8d41dd668760ccddf268
Bozhidar-A <bplatanasov@gmail.com>
2025-02-07 00:22:55 +0200

    testing utils updated

--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -1,12 +1,30 @@
+import { ThemeProvider } from '@/components/ThemeProvider/ThemeProvider';
 import { render as testingLibraryRender } from '@testing-library/react';
-// import { MantineProvider } from '@mantine/core';
-// import { theme } from '../theme';
+import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
+
+//defaults
+import { persistor, store } from "@/store/store";
 
 export function render(ui: React.ReactNode) {
   return testingLibraryRender(<>{ui}</>, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
-      // <MantineProvider theme={theme}>{children}</MantineProvider>
-      <>{children}</>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+      >{children}</ThemeProvider>
     ),
   });
+}
+
+//full redux testing with prod defaults
+export function renderWithProviders(ui: React.ReactNode, objectExtra: {} = {}, mockStore: any = store, mockPersistor: any = persistor) {
+  return testingLibraryRender(
+    <Provider store={mockStore}>
+      <PersistGate loading={<h1>Loading...</h1>} persistor={mockPersistor}>
+        {ui}
+      </PersistGate>
+    </Provider>, objectExtra
+  )
 }
