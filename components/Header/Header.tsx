d6635e6846cd048ab63e3e8d0988e1264ef9032f
Bozhidar Atanasov <bplatanasov@gmail.com>
2025-03-05 10:01:38 +0200

    replaced alerts with toasts, added back forced nav on auth

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,80 +1,106 @@
 'use client';
 
-import Link from "next/link";
-import { useState } from "react";
-import { ThemeSwitcher } from "@/components/ThemeSwitcher/ThemeSwitcher";
-import { Button } from "@/components/ui/button";
-import { Sheet, SheetTrigger, SheetContent, SheetTitle } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
-import { useDispatch, useSelector } from "react-redux";
-import { AuthLogout } from "@/store/slices/authSLice";
-import { BackendLogout } from "@/lib/auth-utils";
-import { Log } from "@/lib/logger";
+import { useState } from 'react';
+import Link from 'next/link';
+import { Menu } from 'lucide-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'sonner';
+import { ThemeSwitcher } from '@/components/ThemeSwitcher/ThemeSwitcher';
+import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { BackendLogout } from '@/lib/auth-utils';
+import { Log } from '@/lib/logger';
+import { AuthLogout } from '@/store/slices/authSLice';
 
 export default function Header() {
-    const authObj = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
+  const authObj = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    async function HandleLogout() {
-        const backendWork = await BackendLogout(authObj.user.id);
+  async function HandleLogout() {
+    const backendWork = await BackendLogout(authObj.user.id);
 
-        if (backendWork.success) {
-            dispatch(AuthLogout());
-            return;
-        }
-
-        Log(["auth", "logout"], backendWork.message);
+    if (backendWork.success) {
+      dispatch(AuthLogout());
+      toast.success('Logged out successfully');
+      return;
     }
 
-    return (
-        <header className="w-full py-4 shadow-md shadow-green-500/50 h-max-20vh">
-            <div className="container mx-auto flex justify-between items-center px-6">
-                <Link href="/"><h1 className="text-2xl font-bold text-green-500">Agro-Calc</h1></Link>
+    Log(['auth', 'logout'], backendWork.message);
+  }
 
-                <div className="flex items-center space-x-4">
-                    <ThemeSwitcher />
+  return (
+    <header className="w-full py-4 shadow-md shadow-green-500/50 h-max-20vh">
+      <div className="container mx-auto flex justify-between items-center px-6">
+        <Link href="/">
+          <h1 className="text-2xl font-bold text-green-500">Agro-Calc</h1>
+        </Link>
 
-                    <Sheet open={open} onOpenChange={setOpen}>
-                        <SheetTrigger asChild data-testid="open-sheet-button">
-                            <Button variant="ghost" size="icon" name="menu">
-                                <Menu className="h-6 w-6" />
-                                <span className="sr-only">Menu Icon</span>
-                            </Button>
-                        </SheetTrigger>
+        <div className="flex items-center space-x-4">
+          <ThemeSwitcher />
 
-                        <SheetContent data-testid="sheet-content">
-                            {authObj.isAuthenticated ? <SheetTitle>Welcome, {authObj.user.email}</SheetTitle> : <SheetTitle>Menu</SheetTitle>}
+          <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild data-testid="open-sheet-button">
+              <Button variant="ghost" size="icon" name="menu">
+                <Menu className="h-6 w-6" />
+                <span className="sr-only">Menu Icon</span>
+              </Button>
+            </SheetTrigger>
 
-                            <div className="flex flex-col space-y-6 mt-6">
-                                {
-                                    authObj.isAuthenticated ? (
-                                        <Button onClick={() => {
-                                            HandleLogout();
-                                            setOpen(false);
-                                        }}>
-                                            Logout
-                                        </Button>
-                                    ) : (
-                                        <div className="flex flex-row justify-center space-x-4">
-                                            <Button asChild>
-                                                <Link href="/auth/login" className="hover:underline" onClick={() => setOpen(false)}>Login</Link>
-                                            </Button>
-                                            <Button asChild>
-                                                <Link href="/auth/register" className="hover:underline" onClick={() => setOpen(false)}>Register</Link>
-                                            </Button>
-                                        </div>
-                                    )
-                                }
-                                <Link href="/" className="hover:underline" onClick={() => setOpen(false)}>Home</Link>
-                                <Link href="/idk" className="hover:underline" onClick={() => setOpen(false)}>NOT prot</Link>
-                                <Link href="/prot" className="hover:underline" onClick={() => setOpen(false)}>prot</Link>
-                            </div>
-                        </SheetContent>
-                    </Sheet>
-                </div>
-            </div>
-        </header>
-    );
+            <SheetContent data-testid="sheet-content">
+              {authObj.isAuthenticated ? (
+                <SheetTitle>Welcome, {authObj.user.email}</SheetTitle>
+              ) : (
+                <SheetTitle>Menu</SheetTitle>
+              )}
+
+              <div className="flex flex-col space-y-6 mt-6">
+                {authObj.isAuthenticated ? (
+                  <Button
+                    onClick={() => {
+                      HandleLogout();
+                      setOpen(false);
+                    }}
+                  >
+                    Logout
+                  </Button>
+                ) : (
+                  <div className="flex flex-row justify-center space-x-4">
+                    <Button asChild>
+                      <Link
+                        href="/auth/login"
+                        className="hover:underline"
+                        onClick={() => setOpen(false)}
+                      >
+                        Login
+                      </Link>
+                    </Button>
+                    <Button asChild>
+                      <Link
+                        href="/auth/register"
+                        className="hover:underline"
+                        onClick={() => setOpen(false)}
+                      >
+                        Register
+                      </Link>
+                    </Button>
+                  </div>
+                )}
+                <Link href="/" className="hover:underline" onClick={() => setOpen(false)}>
+                  Home
+                </Link>
+                <Link href="/idk" className="hover:underline" onClick={() => setOpen(false)}>
+                  NOT prot
+                </Link>
+                <Link href="/prot" className="hover:underline" onClick={() => setOpen(false)}>
+                  prot
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
 }
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,34 +1,37 @@
 export function register() {
-    // On start up, run this function
+  // On start up, run this function
 
-    //check all ENVs are set
-    if (!process.env.DATABASE_URL) {
-        console.error("DATABASE_URL not set");
-        process.exit(1);
-    }
+  //check all ENVs are set
+  if (!process.env.DATABASE_URL) {
+    console.error('DATABASE_URL not set');
+    process.exit(1);
+  }
 
-    if (!process.env.NEXT_PUBLIC_HOST_URL) {
-        console.error("NEXT_PUBLIC_HOST_URL not set");
-        process.exit(1);
-    }
+  if (!process.env.NEXT_PUBLIC_HOST_URL) {
+    console.error('NEXT_PUBLIC_HOST_URL not set');
+    process.exit(1);
+  }
 
-    if (!process.env.JWT_SECRET) {
-        console.error("JWT_SECRET not set");
-        process.exit(1);
-    }
+  if (!process.env.JWT_SECRET) {
+    console.error('JWT_SECRET not set');
+    process.exit(1);
+  }
 
-    if (!process.env.JWT_REFRESH_SECRET) {
-        console.error("JWT_REFRESH_SECRET not set");
-        process.exit(1);
-    }
+  if (!process.env.JWT_REFRESH_SECRET) {
+    console.error('JWT_REFRESH_SECRET not set');
+    process.exit(1);
+  }
 
-    if (!process.env.SALT_ROUNDS) {
-        console.error("SALT_ROUNDS not set");
-        process.exit(1);
-    }
+  if (!process.env.SALT_ROUNDS) {
+    console.error('SALT_ROUNDS not set');
+    process.exit(1);
+  }
 
-    if (!process.env.INTERNAL_API_REQUEST_SECRET) {
-        console.error("INTERNAL_API_REQUEST_SECRET not set");
-        process.exit(1);
-    }
+  if (!process.env.INTERNAL_API_REQUEST_SECRET) {
+    console.error('INTERNAL_API_REQUEST_SECRET not set');
+    process.exit(1);
+  }
+
+  //set up db
+  //npx prisma migrate dev in terminal
 }
