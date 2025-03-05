aa855343c59844a85e903a8959f58d43e6182101
Bozhidar Atanasov <bplatanasov@gmail.com>
2025-03-05 09:56:43 +0200

    fixed possible expired refresh token crash

--- lib/auth-utils.ts
+++ lib/auth-utils.ts
@@ -1,210 +1,215 @@
 'use server';
 
-import { MUTATIONS, QUERIES } from "@/app/api/graphql/callable";
-import { jwtVerify, SignJWT } from "jose";
-import { cookies } from "next/headers";
-import { Log } from "./logger";
-import { GraphQLCaller } from "@/app/api/graphql/graphql-utils";
+import { cookies } from 'next/headers';
+import { jwtVerify, SignJWT } from 'jose';
+import { MUTATIONS, QUERIES } from '@/app/api/graphql/callable';
+import { GraphQLCaller } from '@/app/api/graphql/graphql-utils';
+import { Log } from './logger';
+
 export async function BackendVerifyToken(secret: string, token: string, type: string) {
+  try {
     if (!token) {
-        return [false, null];
+      return [false, null];
     }
 
     // Verify refresh token
     const decoded = await jwtVerify(token, new TextEncoder().encode(secret));
 
     if (decoded?.payload?.type !== type) {
-        console.error("Invalid token type")
-        return [false, null];
+      console.error('Invalid token type');
+      return [false, null];
     }
 
     //also check if it exists in the db
-    if (type === "refresh") {
-        const graphqlData = await GraphQLCaller([
-            "auth",
-            "BackendVerifyToken",
-            "refresh",
-            "graphql"
-        ], QUERIES.REFRESH_TOKEN, {
-            token
-        });
-
-        if (!graphqlData.success) {
-            return [false, null];
+    if (type === 'refresh') {
+      const graphqlData = await GraphQLCaller(
+        ['auth', 'BackendVerifyToken', 'refresh', 'graphql'],
+        QUERIES.REFRESH_TOKEN,
+        {
+          token,
         }
+      );
+
+      if (!graphqlData.success) {
+        return [false, null];
+      }
     }
 
     return [true, decoded];
+  } catch (error) {
+    Log(['auth', 'verifyToken'], `BackendVerifyToken failed with: ${error.message}`);
+    return [false, null];
+  }
 }
 
 export async function BackendRegister(email: string, password: string) {
-    try {
-        const registerUserData = await GraphQLCaller([
-            "auth",
-            "register",
-            "graphql"
-        ], MUTATIONS.HANDLE_REGISTER_ATTEMPT, {
-            email,
-            password
-        })
+  try {
+    const registerUserData = await GraphQLCaller(
+      ['auth', 'register', 'graphql'],
+      MUTATIONS.HANDLE_REGISTER_ATTEMPT,
+      {
+        email,
+        password,
+      }
+    );
 
-        if (!registerUserData.success) {
-            return { success: false, message: "User already exists" };
-        }
-
-        return { success: true, user: { id: registerUserData.data.HandleRegisterAttempt.id, email: registerUserData.data.HandleRegisterAttempt.email } };
-    } catch (error) {
-        Log([
-            "auth",
-            "register"
-        ], `BackendRegister failed with: ${error.message}`)
-        return { success: false, message: error.message };
+    if (!registerUserData.success) {
+      return { success: false, message: 'User already exists' };
     }
+
+    return {
+      success: true,
+      user: {
+        id: registerUserData.data.HandleRegisterAttempt.id,
+        email: registerUserData.data.HandleRegisterAttempt.email,
+      },
+    };
+  } catch (error) {
+    Log(['auth', 'register'], `BackendRegister failed with: ${error.message}`);
+    return { success: false, message: error.message };
+  }
 }
 
 export async function BackendLogin(email: string, password: string) {
-    try {
-        const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-        const loginUserData = await GraphQLCaller([
-            "auth",
-            "login",
-            "graphql"
-        ], MUTATIONS.HANDLE_LOGIN_ATTEMPT, {
-            email,
-            password
-        })
+    const loginUserData = await GraphQLCaller(
+      ['auth', 'login', 'graphql'],
+      MUTATIONS.HANDLE_LOGIN_ATTEMPT,
+      {
+        email,
+        password,
+      }
+    );
 
-        if (!loginUserData.success) {
-            return { success: false, message: "Invalid email or password" };
-        }
-
-        const accessToken = await new SignJWT({ userId: loginUserData.data.HandleLoginAttempt.user.id, type: 'access' })
-            .setProtectedHeader({ alg: 'HS256' })
-            .setExpirationTime('15m')
-            .sign(new TextEncoder().encode(process.env.JWT_SECRET));
-
-        Log([
-            "auth",
-            "login"
-        ], `accessToken: ${accessToken}`);
-
-        // Set cookies
-        cookieStore.set('accessToken', accessToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 900, // 15 minutes
-            path: '/'
-        })
-
-        cookieStore.set('refreshToken', loginUserData.data.HandleLoginAttempt.refreshToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60, // 7 days
-            // path: '/auth/refresh'
-            // apperently it is bad paractice to set to /
-            // will do for dev
-            //TODO: figure out and fix
-            path: '/'
-        })
-
-        cookieStore.set('userId', loginUserData.data.HandleLoginAttempt.user.id, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60, // 7 days
-            path: '/'
-        })
-
-        Log([
-            "auth",
-            "login"
-        ], `Logged in user ${loginUserData.data.HandleLoginAttempt.user.id}`);
-
-        return { success: true, user: { id: loginUserData.data.HandleLoginAttempt.user.id, email: loginUserData.data.HandleLoginAttempt.user.email } };
-    } catch (error) {
-        Log([
-            "auth",
-            "login"
-        ], `BackendLogin failed with: ${error.message}`)
-        return { success: false, message: error.message };
+    if (!loginUserData.success) {
+      return { success: false, message: 'Invalid email or password' };
     }
+
+    const accessToken = await new SignJWT({
+      userId: loginUserData.data.HandleLoginAttempt.user.id,
+      type: 'access',
+    })
+      .setProtectedHeader({ alg: 'HS256' })
+      .setExpirationTime('15m')
+      .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+
+    Log(['auth', 'login'], `accessToken: ${accessToken}`);
+
+    // Set cookies
+    cookieStore.set('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 900, // 15 minutes
+      path: '/',
+    });
+
+    cookieStore.set('refreshToken', loginUserData.data.HandleLoginAttempt.refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60, // 7 days
+      // path: '/auth/refresh'
+      // apperently it is bad paractice to set to /
+      // will do for dev
+      //TODO: figure out and fix
+      path: '/',
+    });
+
+    cookieStore.set('userId', loginUserData.data.HandleLoginAttempt.user.id, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60, // 7 days
+      path: '/',
+    });
+
+    Log(['auth', 'login'], `Logged in user ${loginUserData.data.HandleLoginAttempt.user.id}`);
+
+    return {
+      success: true,
+      user: {
+        id: loginUserData.data.HandleLoginAttempt.user.id,
+        email: loginUserData.data.HandleLoginAttempt.user.email,
+      },
+    };
+  } catch (error) {
+    Log(['auth', 'login'], `BackendLogin failed with: ${error.message}`);
+    return { success: false, message: error.message };
+  }
 }
 
 export async function BackendLogout(userId: string) {
-    try {
-        const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-        const refreshTokenVal = cookieStore.get('refreshToken')?.value;
+    const refreshTokenVal = cookieStore.get('refreshToken')?.value;
 
-        if (!refreshTokenVal) {
-            // No refresh token found
-            //VERY wierd and bad
-            //should never happen
-            //still log em out
-            //an internal error/state desync error should not cause an sao incident
-            //its also bloody stupid
-            Log([
-                "auth",
-                "logout"
-            ], "No refresh token found")
-        }
-
-        const logoutUserData = await GraphQLCaller([
-            "auth",
-            "logout",
-            "graphql"
-        ], MUTATIONS.HANDLE_LOGOUT_ATTEMPT, {
-            token: cookieStore.get('refreshToken')?.value || "",
-            userId
-        })
-
-        if (!logoutUserData.success) {
-            return { success: false, message: "Failed to logout" };
-        }
-
-        // Clear cookies
-        cookieStore.delete('accessToken')
-        cookieStore.delete('refreshToken')
-        cookieStore.delete('userId')
-
-        return { success: true };
+    if (!refreshTokenVal) {
+      // No refresh token found
+      //VERY wierd and bad
+      //should never happen
+      //still log em out
+      //an internal error/state desync error should not cause an sao incident
+      //its also bloody stupid
+      Log(['auth', 'logout'], 'No refresh token found');
     }
-    catch (error) {
-        Log([
-            "auth",
-            "logout"
-        ], `BackendLogout failed with: ${error.message}`)
-        return { success: false, message: error.message };
+
+    const logoutUserData = await GraphQLCaller(
+      ['auth', 'logout', 'graphql'],
+      MUTATIONS.HANDLE_LOGOUT_ATTEMPT,
+      {
+        token: cookieStore.get('refreshToken')?.value || '',
+        userId,
+      }
+    );
+
+    if (!logoutUserData.success) {
+      return { success: false, message: 'Failed to logout' };
     }
+
+    // Clear cookies
+    cookieStore.delete('accessToken');
+    cookieStore.delete('refreshToken');
+    cookieStore.delete('userId');
+
+    return { success: true };
+  } catch (error) {
+    Log(['auth', 'logout'], `BackendLogout failed with: ${error.message}`);
+    return { success: false, message: error.message };
+  }
 }
 
 export async function BackendRefreshAccessToken() {
-    const cookieStore = await cookies();
-    const refreshToken = cookieStore.get('refreshToken')?.value;
+  const cookieStore = await cookies();
+  const refreshToken = cookieStore.get('refreshToken')?.value;
 
-    if (!refreshToken) {
-        throw new Error('No refresh token found');
-    }
+  if (!refreshToken) {
+    throw new Error('No refresh token found');
+  }
 
-    const [validToken, decoded] = await BackendVerifyToken(process.env.JWT_REFRESH_SECRET || '', refreshToken, 'refresh');
-    if (!validToken) {
-        throw new Error('Invalid refresh token');
-    }
+  const [validToken, decoded] = await BackendVerifyToken(
+    process.env.JWT_REFRESH_SECRET || '',
+    refreshToken,
+    'refresh'
+  );
+  if (!validToken) {
+    throw new Error('Invalid refresh token');
+  }
 
-    // Generate new access token
-    const newAccessToken = await new SignJWT({ userId: decoded?.payload?.userId, type: 'access' })
-        .setProtectedHeader({ alg: 'HS256' })
-        .setExpirationTime('15m')
-        .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+  // Generate new access token
+  const newAccessToken = await new SignJWT({ userId: decoded?.payload?.userId, type: 'access' })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setExpirationTime('15m')
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
 
-    // Set new access token cookie
-    cookieStore.set('accessToken', newAccessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: 900, // 15 minutes
-    });
+  // Set new access token cookie
+  cookieStore.set('accessToken', newAccessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 900, // 15 minutes
+  });
 }
