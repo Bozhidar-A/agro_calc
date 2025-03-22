dea9f69513b077ca2fced7cf3f2a25f0e7d0756e
Bozhidar Atanasov <bplatanasov@gmail.com>
2025-03-22 20:31:32 +0200

    fix for login and register form not showing zod errors, foxed backend not checking email and pass creation rules

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
+import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -10,9 +11,8 @@ import { z } from 'zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { AuthFailure, AuthLogout, AuthStart, AuthSuccess } from '@/store/slices/authSLice';
 import { APICaller } from '@/lib/api-util';
-import Link from 'next/link';
+import { AuthFailure, AuthLogout, AuthStart, AuthSuccess } from '@/store/slices/authSlice';
 
 const schema = z.object({
   email: z.string().email('Invalid email address'),
@@ -43,12 +43,13 @@ export default function Login() {
     formState: { errors },
   } = useForm({
     resolver: zodResolver(schema),
+    mode: 'onChange',
   });
 
   async function HandleSubmit(data) {
     dispatch(AuthStart('login'));
 
-    const backendWork = await APICaller(['auth', 'login'], '/api/auth/login', "POST", data);
+    const backendWork = await APICaller(['auth', 'login'], '/api/auth/login', 'POST', data);
 
     if (!backendWork.success) {
       dispatch(AuthFailure(backendWork.message));
@@ -67,7 +68,7 @@ export default function Login() {
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh)] text-center">
       <div className="w-full max-w-md space-y-6 p-8">
         <h2 className="text-2xl font-bold text-center">Login</h2>
-        <form onSubmit={handleSubmit(HandleSubmit)} className="space-y-4">
+        <form onSubmit={handleSubmit(HandleSubmit)} className="space-y-4" noValidate>
           <div>
             <Label htmlFor="email">Email</Label>
             <Input id="email" type="email" {...register('email')} />
