90c216569970bd3d877753cc9d2fbb87ad000c41
Bozhidar-A <bplatanasov@gmail.com>
2025-05-07 03:09:12 +0300

    replaced decare with Acre

--- prisma/migrations/20250316025320_seeding_history/migration.sql
+++ prisma/migrations/20250316025320_seeding_history/migration.sql
@@ -4,16 +4,15 @@ CREATE TABLE "SowingRateHistory" (
     "plantId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "sowingRateSafeSeedsPerMeterSquared" DOUBLE PRECISION NOT NULL,
-    "sowingRatePlantsPerDecare" DOUBLE PRECISION NOT NULL,
-    "usedSeedsKgPerDecare" DOUBLE PRECISION NOT NULL,
+    "sowingRatePlantsPerAcre" DOUBLE PRECISION NOT NULL,
+    "usedSeedsKgPerAcre" DOUBLE PRECISION NOT NULL,
     "internalRowHeightCm" DOUBLE PRECISION NOT NULL,
     "isDataValid" BOOLEAN NOT NULL,
-
     CONSTRAINT "SowingRateHistory_pkey" PRIMARY KEY ("id")
 );
-
 -- AddForeignKey
-ALTER TABLE "SowingRateHistory" ADD CONSTRAINT "SowingRateHistory_plantId_fkey" FOREIGN KEY ("plantId") REFERENCES "Plant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
+ALTER TABLE "SowingRateHistory"
+ADD CONSTRAINT "SowingRateHistory_plantId_fkey" FOREIGN KEY ("plantId") REFERENCES "Plant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 -- AddForeignKey
-ALTER TABLE "SowingRateHistory" ADD CONSTRAINT "SowingRateHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "SowingRateHistory"
+ADD CONSTRAINT "SowingRateHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
