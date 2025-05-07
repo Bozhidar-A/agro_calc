05d98b31ebc688a054244f61d6c0bfa264cdbc0e
Bozhidar-A <bplatanasov@gmail.com>
2025-05-07 04:00:01 +0300

    on sowing added support for hectares

---
+++ components/SowingMeasurementSwitcher/SowingMeasurementSwitcher.tsx
@@ -0,0 +1,51 @@
+import { SowingRateSaveData } from "@/app/hooks/useSowingRateForm";
+import { SELECTABLE_STRINGS } from "@/lib/LangMap";
+import { useTranslate } from "@/app/hooks/useTranslate";
+import { DisplayOutputRow } from "@/app/calculators/sowing/page";
+import { useSelector } from "react-redux";
+import { UNIT_OF_MEASUREMENT_LENGTH } from "@/lib/LocalSettingsMaps";
+import { KgPerAcreToKgPerHectare, SowingRatePlantsPerAcreToHectare, ToFixedNumber } from "@/lib/math-util";
+
+export default function SowingMeasurementSwitcher(dataToBeSaved: SowingRateSaveData) {
+    const translator = useTranslate();
+    const unitOfMeasurement = useSelector((state: any) => state.local.unitOfMeasurementLength);
+
+    switch (unitOfMeasurement) {
+        case UNIT_OF_MEASUREMENT_LENGTH.ACRES:
+            return (
+                <>
+                    <DisplayOutputRow
+                        data={dataToBeSaved.sowingRatePlantsPerAcre}
+                        text={translator(SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_SOWING_RATE)}
+                        unit={translator(
+                            SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_SOWING_RATE_PLANTS_PER_ACRE
+                        )}
+                    />
+                    <DisplayOutputRow
+                        data={dataToBeSaved.usedSeedsKgPerAcre}
+                        text={translator(SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_USED_SEEDS)}
+                        unit={translator(SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_KA_PER_ACRE)}
+                    />
+                </>
+            );
+        case UNIT_OF_MEASUREMENT_LENGTH.HECTARES:
+            return (
+                <>
+                    <DisplayOutputRow
+                        data={ToFixedNumber(SowingRatePlantsPerAcreToHectare(dataToBeSaved.sowingRatePlantsPerAcre), 0)}
+                        text={translator(SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_SOWING_RATE)}
+                        unit={translator(
+                            SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_SOWING_RATE_PLANTS_PER_HECTARE
+                        )}
+                    />
+                    <DisplayOutputRow
+                        data={ToFixedNumber(KgPerAcreToKgPerHectare(dataToBeSaved.usedSeedsKgPerAcre), 2)} // Convert kg/acre to kg/hectare
+                        text={translator(SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_USED_SEEDS)}
+                        unit={translator(SELECTABLE_STRINGS.SOWING_RATE_OUTPUT_KA_PER_HECTARE)}
+                    />
+                </>
+            );
+        default:
+            return null;
+    }
+}
