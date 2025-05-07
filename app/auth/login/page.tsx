d85ecefb4e8f157732718d6c508add1f4777c1d2
Bozhidar-A <bplatanasov@gmail.com>
2025-05-07 03:09:23 +0300

    fixed link to register

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -97,7 +97,7 @@ export default function Login() {
         <div className="flex items-center justify-center space-x-2">
           <h2>{translator(SELECTABLE_STRINGS.NO_ACC_Q)}</h2>
           <Button asChild>
-            <Link href="/auth/login" className="hover:underline">
+            <Link href="/auth/register" className="hover:underline">
               {translator(SELECTABLE_STRINGS.REGISTER)}
             </Link>
           </Button>
