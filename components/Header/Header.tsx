f74705daa6cccaaad87287ce60f77728dff603dc
Bozhidar-A <bplatanasov@gmail.com>
2025-05-22 13:28:39 +0300

    fixed some coloring

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
     <header className="w-full py-4 shadow-md shadow-green-500/50 h-max-20vh">
       <div className="container mx-auto flex justify-between items-center px-6">
         <Link href="/">
-          <h1 className="text-2xl font-bold text-green-600">Agro-Calc</h1>
+          <h1 className="text-2xl font-bold text-green-700">Agro-Calc</h1>
         </Link>
 
         <div className="flex items-center space-x-4">
