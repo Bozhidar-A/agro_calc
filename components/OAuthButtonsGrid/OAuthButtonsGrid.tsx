0f6b74e6def5097b36594dde78b610f946c1e299
Bozhidar-A <bplatanasov@gmail.com>
2025-05-23 15:00:29 +0300

    moved oauth buttons to a component

---
+++ components/OAuthButtonsGrid/OAuthButtonsGrid.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import SimpleIconToSVG from "@/components/SimpleIconToSVG/SimpleIconToSVG";
+import { SUPPORTED_OAUTH_PROVIDERS_TYPE } from "@/lib/types";
+import { SUPPORTED_OAUTH_PROVIDERS } from "@/lib/utils";
+
+
+function OAuthButton({ buttonData }: { buttonData: SUPPORTED_OAUTH_PROVIDERS_TYPE }) {
+    return (
+        <Button asChild className="text-black dark:text-white font-bold">
+            <a href={buttonData.authURL}>
+                <SimpleIconToSVG icon={buttonData.icon} />
+            </a>
+        </Button>
+    )
+}
+
+export default function OAuthButtonsGrid() {
+    return (
+        <div className="flex flex-wrap justify-center gap-2">
+            {Object.values(SUPPORTED_OAUTH_PROVIDERS).map((buttonData) => (
+                <OAuthButton key={buttonData.name} buttonData={buttonData} />
+            ))}
+        </div>
+    );
+}
