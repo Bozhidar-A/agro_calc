aa87148f30e9205f4fc631bba08c1bbfbcb11f5d
Bozhidar-A <bplatanasov@gmail.com>
2025-05-24 01:00:06 +0300

    fixed not allowed duplicate emails with OAuth

--- app/api/auth/login/google/callback/route.ts
+++ app/api/auth/login/google/callback/route.ts
@@ -1,7 +1,7 @@
 import { google } from "@/lib/oauth-utils";
 import { cookies } from "next/headers";
 import { decodeIdToken } from "arctic";
-import { CreateUserGoogle, DeleteAllRefreshTokensByUserId, FindUserByGoogleId, InsertRefreshTokenByUserId } from "@/prisma/prisma-utils";
+import { AttachGoogleIdToUser, CreateUserGoogle, DeleteAllRefreshTokensByUserId, FindUserByEmail, FindUserByGoogleId, InsertRefreshTokenByUserId } from "@/prisma/prisma-utils";
 import { NextResponse } from "next/server";
 import { SignJWT } from "jose";
 import { Log } from "@/lib/logger";
@@ -42,15 +42,28 @@ export async function GET(request: Request) {
         const userId = claims.sub;
         const email = claims.email;
 
-        Log(["auth", "login", "google", "callback"], `Claims: ${JSON.stringify(claims)}`);
-        Log(["auth", "login", "google", "callback"], `User ID: ${userId}`);
-        Log(["auth", "login", "google", "callback"], `Email: ${email}`);
+        Log(["auth", "login", "google", "callback"], `Google ID: ${userId}`);
+        Log(["auth", "login", "google", "callback"], `Google Email: ${email}`);
 
+        Log(["auth", "login", "google", "callback"], `Finding user by Google ID: ${userId}`);
         let user = await FindUserByGoogleId(userId);
         if (!user) {
-            user = await CreateUserGoogle(userId, email);
-            Log(["auth", "login", "google", "callback"], `Created new user: ${user.id}`);
+
+            Log(["auth", "login", "google", "callback"], `User not found, finding user by email: ${email}`);
+            const existingUser = await FindUserByEmail(email);
+
+            if (existingUser) {
+                Log(["auth", "login", "google", "callback"], `User already exists, attaching Google ID to user: ${existingUser.id}`);
+                await AttachGoogleIdToUser(existingUser.id, userId);
+                user = existingUser;
+            } else {
+                Log(["auth", "login", "google", "callback"], `Creating new user: ${email}`);
+                user = await CreateUserGoogle(userId, email);
+                Log(["auth", "login", "google", "callback"], `Created new user: ${user.id}`);
+            }
         }
+
+        Log(["auth", "login", "google", "callback"], `User: ${JSON.stringify(user?.id)}`);
 
         //nuke all old refresh tokens
         const deletedTokensCount = await DeleteAllRefreshTokensByUserId(user.id);
