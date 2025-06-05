9fde341f23d7f3ec87dcecce00e2cebfddfe213c
Bozhidar-A <bplatanasov@gmail.com>
2025-06-06 01:07:44 +0300

    warning to history display

--- app/calculators/chemical-protection/working-solution/page.tsx
+++ app/calculators/chemical-protection/working-solution/page.tsx
@@ -195,7 +195,6 @@ export default function ChemicalProtectionWorkingSolution() {
 
                                         )
                                     }
-
                                 </div>
                             )}
                         </form>
