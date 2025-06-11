ab97015407001cc882a6dc2012c4b56771ae28ff
Bozhidar-A <bplatanasov@gmail.com>
2025-06-12 02:48:01 +0300

    types for useTranslate

--- hooks/useTranslate.ts
+++ hooks/useTranslate.ts
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import { GetStrFromLangMapKey } from '@/lib/utils';
+import { RootState } from '@/store/store';
 
 export function useTranslate() {
-  const lang = useSelector((state) => state.local.lang);
+  const lang = useSelector((state: RootState) => state.local.lang);
 
   return (key: string) => GetStrFromLangMapKey(lang, key);
 }
